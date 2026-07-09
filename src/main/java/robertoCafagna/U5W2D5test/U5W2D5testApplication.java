6a365d49a3d3f2074cb9d78b021d9a90ab5fa911
cafagnarob <cafagna.rob@gmail.com>
2026-07-09 17:15:28 +0200

    Initial commit

---
+++ src/main/java/robertoCafagna/U5W2D5test/U5W2D5testApplication.java
@@ -0,0 +1,13 @@
+package robertoCafagna.U5W2D5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D5testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D5testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/robertoCafagna/U5W2D5test/U5W2D5testApplicationTests.java
@@ -0,0 +1,13 @@
+package robertoCafagna.U5W2D5test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
